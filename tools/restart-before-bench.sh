1be8ed7fa8e74adf36d75691aa2e08cae05482cd
skkzsh <skkzsh12@gmail.com>
2022-07-23 02:24:07 +0000

    Update script

--- tools/restart-before-bench.sh
+++ tools/restart-before-bench.sh
@@ -7,8 +7,11 @@ sudo truncate -s 0 -c /var/log/mysql/mysql-slow.log
 sudo truncate -s 0 -c /var/log/mysql/error.log
 # mysqladmin flush-logs
 
+# GOPATH=/home/isucon/tmp/go
+# GOCACHE=/home/isucon/tmp/go/.cache
 # cd ~/webapp/go
 # make
+
 docker compose -f ~/webapp/docker-compose-go.yml build
 sudo systemctl restart isuports.service
 
