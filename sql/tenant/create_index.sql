ef555db2249d841dd980c1eb946b12b1dd4f7df4
skkzsh <skkzsh12@gmail.com>
2024-11-23 16:48:32 +0900

    ANALYZE #1

--- sql/tenant/create_index.sql
+++ sql/tenant/create_index.sql
@@ -1,2 +1,4 @@
 CREATE INDEX idx_player_score_tenant_competition_player_rownum
-  ON player_score (tenant_id, competition_id, player_id, row_num DESC);
+    ON player_score (tenant_id, competition_id, player_id, row_num DESC);
+
+ANALYZE;
