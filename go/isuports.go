8f2c23e9d9aad77afc69328fdf9139f4ce404201
skkzsh <skkzsh12@gmail.com>
2024-11-24 22:26:21 +0900

    visit_history は最古データしか参照していないので, insertは初回だけにする #1

--- go/isuports.go
+++ go/isuports.go
@@ -1524,10 +1524,13 @@ func competitionRankingHandler(c echo.Context) error {
 		return fmt.Errorf("error Select tenant: id=%d, %w", v.tenantID, err)
 	}
 
-	if _, err := adminDB.ExecContext( // TODO: select visit_history の高速化のため, 初回だけinsertするといいかも?
+	if _, err := adminDB.ExecContext(
 		ctx,
-		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
-		v.playerID, tenant.ID, competitionID, now, now,
+		// "INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
+		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) SELECT ?, ?, ?, ?, ?"+
+			" WHERE NOT EXISTS (SELECT 1 FROM visit_history WHERE tenant_id =? AND competition_id = ? AND player_id = ?)",
+		// visit_history は最古データしか参照していないので, insertは初回だけにする
+		v.playerID, tenant.ID, competitionID, now, now, tenant.ID, competitionID, v.playerID,
 	); err != nil {
 		return fmt.Errorf(
 			"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
